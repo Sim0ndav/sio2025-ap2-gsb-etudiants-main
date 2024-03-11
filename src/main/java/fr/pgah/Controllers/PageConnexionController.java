3dabb7529bc8068afd1913ef8a160aae34293348
Sim0ndav <si.char.david@gmail.com>
2024-03-11 15:41:49 +0100

    test

--- src/main/java/fr/pgah/Controllers/PageConnexionController.java
+++ src/main/java/fr/pgah/Controllers/PageConnexionController.java
@@ -1,5 +1,13 @@
 package fr.pgah.Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class PageConnexionController {
+
+    @FXML
+    void btnConnexion(ActionEvent event) {
+
+    }
 
 }
