88ee6086c3a242050c280c51cdb5b425ef70c1d4
Sim0ndav <si.char.david@gmail.com>
2024-03-22 15:52:27 +0100

    update

--- src/main/java/fr/pgah/Controllers/PageConnexionController.java
+++ src/main/java/fr/pgah/Controllers/PageConnexionController.java
@@ -1,11 +1,11 @@
 package fr.pgah.Controllers;
 
 import fr.pgah.AccesBdd.AccesBdd;
-import fr.pgah.Logger.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 public class PageConnexionController {
 
@@ -19,18 +19,25 @@ public class PageConnexionController {
     private TextField id_password;
 
     @FXML
+    private Text messageErreur;
+
+    @FXML
     void btnConnexion(ActionEvent event) {
 
         String identifiant = id_login.getText();
         String motDePasse = id_password.getText();
 
+        // Utilisation de la méthode de vérification des identifiants de la classe
+        // AccesBdd
         boolean identifiantsValides = AccesBdd.verifierIdentifiants(identifiant, motDePasse);
 
         if (identifiantsValides) {
-            Logger.log("Connexion réussie !");
+            // Rediriger l'utilisateur vers la page suivante
+            messageErreur.setText("Connexion réussie !");
+            // Code pour changer de scène ou effectuer d'autres actions après la connexion
+            // réussie
         } else {
-            Logger.log("Identifiant ou mot de passe incorrect !");
+            messageErreur.setText("Identifiant ou mot de passe incorrect !");
         }
-
     }
 }
